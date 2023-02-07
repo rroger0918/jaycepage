0c40198be1c3154fa16dc38dbf82be44a4a251e0
Jayce0918 <51176006+Jayce0918@users.noreply.github.com>
2023-02-08 01:38:53 +0800

    聯繫頁Bug修正完成

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,68 +1,70 @@
-
-
-  (function () {
+(function() {
     // emailjs.init("USERID");
     emailjs.init("user_Ec3EGOLiAx79C0pwx9cEw");
-  })();
+})();
 
-  /**
-    emailjs.send("SERVICE ID", "TEMPLATE NAME", {
-      to_name: "USERNAME",
-      from_name: "FROM NAME",
-      message: "MESSAGE",
-    });
-   **/
+/**
+  emailjs.send("SERVICE ID", "TEMPLATE NAME", {
+    to_name: "USERNAME",
+    from_name: "FROM NAME",
+    message: "MESSAGE",
+  });
+ **/
 
-  function validate() {
+function validate() {
     let loader = document.querySelector(".loader");
-    let name = document.querySelector(".username");
-    let email = document.querySelector(".email");   
-    let msg = document.querySelector(".message");
+    let name = document.querySelector(".from_name");
+    let email = document.querySelector(".email");
+    let subject = document.querySelector(".subject");
+    let message = document.querySelector(".message");
     let btn = document.querySelector(".submit");
-    
+
 
     btn.addEventListener("click", (e) => {
-      e.preventDefault();
-      if (name.value == "" || email.value == "" || msg.value == "") {
-        emptyerror();
-      } else {
-        loader.style.display = "flex";
-        sendmail(name.value, email.value, msg.value);
-        success();
-        loader.style.display = "none";
-      }
+        e.preventDefault();
+        if (name.value == "" || email.value == "" || message.value == "") {
+            emptyerror();
+        } else {
+            loader.style.display = "flex";
+            sendmail(name.value, email.value, subject.value, message.value);
+            success();
+            loader.style.display = "none";
+        }
     });
-  }
-  validate();
+}
+validate();
 
-  function sendmail(name, email, msg) {
+function sendmail(name, email, suj, msg) {
     emailjs.send("service_38yxfmh", "template_ot6lgqg", {
-      to_name: "GT, li",
-      from_name: email,
-      message: msg,
+        from_name: name,
+        to_name: "冠霆",
+        email: email,
+        from_name: name,
+        subject: suj,
+        message: msg,
     });
-  }
+}
 
-  function emptyerror() {
+function emptyerror() {
     Swal.fire({
-      icon: "Error ! ",
-      title: "Oops...",
-      text: "The message cannot be blank ~",
+        icon: "Error ! ",
+        title: "Oops...",
+        text: "The message cannot be blank ~",
     });
-  }
+}
 
-  function error() {
+function error() {
     Swal.fire({
-      icon: "Error !",
-      title: "Oops...",
-      text: "Something is wrong....Please send me a letter : leekuantean@gmail.com ,THX!",
+        icon: "Error !",
+        title: "Oops...",
+        text: "Something is wrong....Please send me a letter : leekuantean@gmail.com ,THX!",
     });
-  }
+}
 
-  function success() {
+function success() {
     Swal.fire({
-      icon: "success",
-      title: "Successfully sent <3",
-      text: "I will reply you as soon as possible ~",
+        icon: "success",
+        title: "Successfully sent <3",
+        text: "I will reply you as soon as possible ~ 謝啦🤘",
     });
-  }
+}
